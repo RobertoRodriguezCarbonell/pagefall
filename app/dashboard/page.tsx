dfc3b06b84f9ac07189a7f94ccec7026bd30757a
Roberto Rodriguez Carbonell <98robers@gmail.com>
2026-01-25 18:12:24 +0100

    Implementation of the sidebar and creation of the page wrapper to wrap the page content.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import { Logout } from "@/components/logout";
+import { PageWrapper } from "@/components/page-wrapper";
 
 export default function Page() {
     return (
-        <div className="px-10 py-8 flex items-center justify-between">
+        <PageWrapper breadcrumbs={[{ label: "Dashboard", href: "/dashboard" }]}>
             <h1>Dashboard</h1>
             <Logout />
-        </div>
+        </PageWrapper>
+
     )
 }
