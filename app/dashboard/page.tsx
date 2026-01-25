15cf3e2da0607cc7d871885066623f84f1654c21
Roberto Rodriguez Carbonell <98robers@gmail.com>
2026-01-25 19:13:51 +0100

    Implementation of notebooks and creation of functionalities for creating, deleting, updating, etc. Implementation of notes and functionality for creating, deleting, and updating notes within notebooks.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,24 @@
-import { Logout } from "@/components/logout";
+import { CreateNotebookButton } from "@/components/create-notebook-button";
 import { PageWrapper } from "@/components/page-wrapper";
+import { getNotebooks } from "@/server/notebooks";
 
-export default function Page() {
+export default async function Page() {
+    const notebooks = await getNotebooks();
+
     return (
         <PageWrapper breadcrumbs={[{ label: "Dashboard", href: "/dashboard" }]}>
-            <h1>Dashboard</h1>
-            <Logout />
+            <h1>Notebooks</h1>
+
+            <CreateNotebookButton />
+
+            {notebooks.success &&
+                notebooks?.notebooks?.map((notebook) => (
+                    <div key={notebook.id}>{notebook.name}</div>
+                ))}
+
+            {notebooks.success && notebooks?.notebooks?.length === 0 && (
+                <div>No notebooks found</div>
+            )}
         </PageWrapper>
 
     )
--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
-import { relations } from "drizzle-orm";
-import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
+import { relations, sql } from "drizzle-orm";
+import { pgTable, text, timestamp, boolean, index, jsonb } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
@@ -73,6 +73,46 @@ export const verification = pgTable(
   (table) => [index("verification_identifier_idx").on(table.identifier)],
 );
 
+export const notebooks = pgTable("notebooks", {
+    id: text('id').primaryKey().default(sql`gen_random_uuid()`),
+    name: text('name').notNull(),
+    userId: text('user_id').notNull().references(() => user.id, { onDelete: 'cascade' }),
+    createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()),
+    updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date())
+});
+
+export const notebookRelations = relations(notebooks, ({ many, one }) => ({
+    notes: many(notes),
+    user: one(user, {
+        fields: [notebooks.userId],
+        references: [user.id]
+    })
+}));
+
+export type Notebook = typeof notebooks.$inferSelect & {
+    notes: Note[];
+};
+export type InsertNotebook = typeof notebooks.$inferInsert;
+
+export const notes = pgTable("notes", {
+    id: text('id').primaryKey().default(sql`gen_random_uuid()`),
+    title: text('title').notNull(),
+    content: jsonb('content').notNull(),
+    notebookId: text('notebook_id').notNull().references(() => notebooks.id, { onDelete: 'cascade' }),
+    createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()),
+    updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date())
+});
+
+export const noteRelations = relations(notes, ({ one }) => ({
+    notebook: one(notebooks, {
+        fields: [notes.notebookId],
+        references: [notebooks.id]
+    })
+}));
+
+export type Note = typeof notes.$inferSelect;
+export type InsertNote = typeof notes.$inferInsert;
+
 export const userRelations = relations(user, ({ many }) => ({
   sessions: many(session),
   accounts: many(account),
@@ -92,4 +132,4 @@ export const accountRelations = relations(account, ({ one }) => ({
   }),
 }));
 
-export const schema = { user, session, account, verification };
+export const schema = { user, session, account, verification, notebooks, notes, notebookRelations, noteRelations };
